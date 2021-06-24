a340b75f4fedcfeb2716b0021bfa2977b331b19f
saeedkp <kp_saeed1@yahoo.com>
2021-06-24 05:18:54 +0430

    Remove unused methods in Offering class

--- src/domain/Offering.java
+++ src/domain/Offering.java
@@ -10,18 +10,6 @@ public class Offering extends Course{
 		this.section = 1;
 		this.examDate = null;
 	}
-
-	public Offering(String id, String name, int units, Date examDate) {
-		super(id, name, units);
-		this.section = 1;
-		this.examDate = examDate;
-	}
-
-	public Offering(String id, String name, int units, Date examDate, int section) {
-		super(id, name, units);
-		this.section = section;
-		this.examDate = examDate;
-	}
 	
 	public String toString() {
 		return super.getName() + " - " + section;
@@ -30,8 +18,6 @@ public class Offering extends Course{
 	public Date getExamTime() {
 		return examDate;
 	}
-
-	public int getSection() { return section; }
 
 	public void setExamDate(Date examDate) {
 		this.examDate = examDate;

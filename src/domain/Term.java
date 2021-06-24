b4e2d2152f468bed8e6f6d141688a52b7a3678fc
saeedkp <kp_saeed1@yahoo.com>
2021-06-24 05:26:13 +0430

    Remove unused methods and libraries in Term class

--- src/domain/Term.java
+++ src/domain/Term.java
@@ -12,22 +12,9 @@ public class Term {
 		this.name = name;
 		this.startDate = null;
 	}
-
-	public Term(String name, Date startDate) {
-		this.name = name;
-		this.startDate = startDate;
-	}
-	
-	public String getName() {
-		return name;
-	}
 	
 	public String toString() {
 		return name;
-	}
-
-	public Date getStartDate() {
-		return startDate;
 	}
 
 	public Map<Offering, Double> getOfferings() {

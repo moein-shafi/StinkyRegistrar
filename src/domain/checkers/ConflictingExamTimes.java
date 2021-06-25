5178072e627ba2850f7f559b840f50a855c2f6e4
Sepehr Rafiee <sepehr.rafiee@snapp.cab>
2021-06-25 13:25:07 +0430

    feat: add ConflictingExamTimes checker

---
+++ src/domain/checkers/ConflictingExamTimes.java
@@ -0,0 +1,20 @@
+public class ConflictingExamTimes {
+    List<Offering> offerings;
+
+    public ConflictingExamTimes(List<Offering> offerings){
+        this.offerings = offerings;
+    }
+
+    public void doCheck() throws EnrollmentRulesViolationException {
+        for (Offering firstOffering : offerings) {
+            for (Offering secondOffering : offerings) {
+                if (firstOffering == secondOffering)
+                    continue;
+                if (firstOffering.hasExamTimeConflict(secondOffering.getExamTime()))
+                    throw new EnrollmentRulesViolationException(
+                            String.format("Two offerings %s and %s have the same exam time",
+                                    firstOffering, secondOffering));
+            }
+        }
+    }
+}

090ad5952fde05d5459a3a989d28c8001ec935ef
Sepehr Rafiee <sepehr.rafiee@snapp.cab>
2021-06-25 14:41:54 +0430

    refactor: refactor EnrollmentControl to use violations instead of execptions

--- src/domain/EnrollmentControl.java
+++ src/domain/EnrollmentControl.java
@@ -11,20 +11,33 @@ import domain.checkers.AlreadyPassedCourses;
 import domain.checkers.GPALimit;
 import domain.checkers.DuplicateEnrollRequest;
 import domain.checkers.ConflictingExamTimes;
+import domain.violations.BaseViolation;
 
 public class EnrollmentControl {
-    private List<BaseChecker> checkers;
+    private String violationMsg;
 
 	public void enroll(Student student, List<Offering> offerings) throws EnrollmentRulesViolationException {
+        List<BaseChecker> checkers;
+        List<BaseViolation> violations;
         checkers = new ArrayList<>();
+        violations = new ArrayList<>();
+
         checkers.add(new AlreadyPassedCourses(student, offerings));
         checkers.add(new PrerequisiteRequirements(student, offerings));
         checkers.add(new GPALimit(student, offerings));
         checkers.add(new DuplicateEnrollRequest(offerings));
         checkers.add(new ConflictingExamTimes(offerings));
 
-        for (BaseChecker checker : checkers)
-            checker.doCheck();
+        for (BaseChecker checker : checkers){
+            var violation = checker.doCheck();
+            if (violation != null){
+                violations.add(violation);
+                violationMsg += violation.getViolationMessage();
+            }
+        }
+        if (!violations.isEmpty()){
+            throw new EnrollmentRulesViolationException(violationMsg);
+        }
         for (Offering offering : offerings)
             student.takeOffering(offering);
     }

77f8ea605c7ee5c8226a7464bea2750178783952
Sepehr Rafiee <sepehr.rafiee@snapp.cab>
2021-06-25 13:21:15 +0430

    feat: add DuplicateEnrollRequest checker

---
+++ src/domain/checkers/DuplicateEnrollRequest.java
@@ -0,0 +1,19 @@
+public class DuplicateEnrollRequest {
+    List<Offering> offerings;
+
+    public DuplicateEnrollRequest(List<Offering> offerings){
+        this.offerings = offerings;
+    }
+
+    public void doCheck() throws EnrollmentRulesViolationException {
+        for (Offering firstOffering : offerings) {
+            for (Offering secondOffering : offerings) {
+                if (firstOffering == secondOffering)
+                    continue;
+                if (firstOffering.equals(secondOffering))
+                    throw new EnrollmentRulesViolationException(
+                            String.format("%s is requested to be taken twice", firstOffering.getName()));
+            }
+        }
+    }
+}
